cb43735204e4342620c69880d4262048a719c942
axiss-code <79708816+axiss-code@users.noreply.github.com>
2022-05-10 18:28:01 +0100

    Create ejercicio_2.js

---
+++ JavaScript Básico/Tema 2/ejercicio_2.js
@@ -0,0 +1,17 @@
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+*/
+
+const lista = ["Jorge",45,true,new Date(1977,01,01),
+  {
+   titulo: "Clean Code",
+   autor: "Robert C. Martin",
+   fecha: new Date(2012,05,19),
+   url: "https://https://www.amazon.es/C%C3%B3digo-Limpio-desarrollo-software-Programaci%C3%B3n/dp/8441532109"
+  },
+];
